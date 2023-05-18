f5d142ef11247c0e8baa4e77c46bb1f256da7cdd
maxsaunders8794@gmail.com <maxsaunders8794@gmail.com>
2023-05-18 02:29:54 -0400

    maybe this will fix gh page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Route,
-  Routes,
-  Navigate
+  Switch,
+  Redirect
 } from "react-router-dom";
 import Navbar from './Components/Navbar'
 import PageHome from "./Components/PageHome";
@@ -12,14 +12,20 @@ import './App.css'
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<PageHome />} />
-        <Route path="/resume" element={<PageResume />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Redirect from='*' to='/resume-site' />
+        </Switch>
+      </Router>
+      <Router basename='/resume-site/'>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={PageHome} />
+          <Route exact path="/resume" component={PageResume} />
+        </Switch>
+      </Router>
+    </>
   )
 }
 
