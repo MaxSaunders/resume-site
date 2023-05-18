6262d2c09c689f8a5763df9bf8e9258ba164b0f1
maxsaunders8794@gmail.com <maxsaunders8794@gmail.com>
2023-05-18 02:07:42 -0400

    more styles, gh pages fix

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,60 +1,65 @@
 import { Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineHome, AiFillGithub, AiOutlineMenuFold, AiOutlineMail } from 'react-icons/ai'
 import { CgFileDocument } from 'react-icons/cg'
 import './index.css'
 
-const NavBar = () => {
+const Routes = () => {
+    const { pathname } = useLocation()
+
     return (
-        <Navbar expand="md" className="navbar">
-            <Navbar.Brand>
-                <div className='navbar-logo'>
-                    <div className='logo-wrapper'>
-                        <div className='fw-bold'>
-                            <span className='logo'>
-                                {`</>`}
-                            </span>
-                            <span className='logo-name'>
-                                {`M. S.`}
-                                {/* {`Max Saunders`} */}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarCollapse" >
-                <AiOutlineMenuFold size={40} />
-            </Navbar.Toggle>
-            <Navbar.Collapse id="navbarCollapse">
-                <Nav className="justify-content-evenly w-100">
-                    <Nav.Item>
-                        <Nav.Link className='justify-space-between' as={Link} to='/'>
-                            <AiOutlineHome size='23' className='me-2' />
-                            Home
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className=''>
-                        <Nav.Link className='justify-space-between' as={Link} to='/resume'>
-                            <CgFileDocument size='23' className='me-2' />
-                            Resume
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <a className='nav-link justify-space-between' target='_blank' href='https://github.com/MaxSaunders' rel="noreferrer">
-                            <AiFillGithub size='23' className='me-2' />
-                            Github
-                        </a>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <a className='nav-link' href={`mailto:max@saunders.wiki`}>
-                            <AiOutlineMail size='23' className='me-2' />
-                            Email
-                        </a>
-                    </Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar >
+        <Nav className="justify-content-evenly w-100">
+            <Nav.Item>
+                <Nav.Link active={pathname === '/'} className='justify-space-between' as={Link} to='/'>
+                    <AiOutlineHome size='23' className='me-2' />
+                    Home
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className=''>
+                <Nav.Link active={pathname === '/resume'} className='justify-space-between' as={Link} to='/resume'>
+                    <CgFileDocument size='23' className='me-2' />
+                    Resume
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <a className='link-a nav-link justify-space-between' target='_blank' href='https://github.com/MaxSaunders' rel="noreferrer">
+                    <AiFillGithub size='23' className='me-2' />
+                    Github
+                </a>
+            </Nav.Item>
+            <Nav.Item>
+                <a className='.link-a nav-link' href={`mailto:max@saunders.wiki`}>
+                    <AiOutlineMail size='23' className='me-2' />
+                    Email
+                </a>
+            </Nav.Item>
+        </Nav>
     )
 }
+
+const NavBar = () =>
+    <Navbar expand="md" className="navbar">
+        <Navbar.Brand>
+            <div className='navbar-logo'>
+                <div className='logo-wrapper'>
+                    <div className='fw-bold'>
+                        <span className='logo'>
+                            {`</>`}
+                        </span>
+                        <span className='logo-name'>
+                            {/* {`M. S.`} */}
+                            {`Max Saunders`}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarCollapse" >
+            <AiOutlineMenuFold size={40} />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarCollapse">
+            <Routes />
+        </Navbar.Collapse>
+    </Navbar >
 
 export default NavBar
--- src/Components/PageResume/index.jsx
+++ src/Components/PageResume/index.jsx
@@ -1,10 +1,21 @@
 import { Container, Row, Col } from "react-bootstrap"
 import { Document, Page, pdfjs } from "react-pdf"
+import { useEffect, useState } from "react";
 import Resume from '../../assets/resume.pdf'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PageResume = () => {
+    const [width, setWidth] = useState(500)
+
+    useEffect(() => {
+        setWidth(window.innerWidth)
+
+        window.addEventListener('resize', () => {
+            setWidth(window.innerWidth)
+        }, false)
+    }, [])
+
     return (
         <Container className='mt-3'>
             <Row>
@@ -13,11 +24,16 @@ const PageResume = () => {
                         file={Resume}
                         className="d-flex justify-content-center"
                     >
-                        <Page pageNumber={1} scale={1.49} renderTextLayer={false} renderAnnotationLayer={false} />
+                        <Page
+                            pageNumber={1}
+                            scale={Math.min((width / 650), 1.5)}
+                            renderTextLayer={false}
+                            renderAnnotationLayer={false}
+                        />
                     </Document>
                 </Col>
             </Row>
-        </Container>
+        </Container >
     )
 }
 
