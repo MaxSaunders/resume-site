76a2947b83cf7917f071d23e0570d1301d4586f6
maxsaunders8794@gmail.com <maxsaunders8794@gmail.com>
2023-05-20 15:09:56 -0400

    remove logging

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -12,26 +12,19 @@ import PageResume from '../PageResume';
 import PageProjects from '../PageProjects';
 import './index.css'
 
-const App = () => {
-  console.log(
-    'pathname:  ',
-    window.location.pathname
-  )
-  return (
-    <>
-      <Router basename={window.location.pathname || ''}>
-        {/* TODO: add transitions for page changes */}
-        <Particle />
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={PageHome} />
-          <Route exact path="/resume" component={PageResume} />
-          <Route exact path="/projects" component={PageProjects} />
-          {/* <Route exact path="/*" component={Page401} /> */}
-        </Switch>
-      </Router>
-    </>
-  )
-}
+const App = () =>
+  <>
+    <Router basename={window.location.pathname || ''}>
+      {/* TODO: add transitions for page changes */}
+      <Particle />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={PageHome} />
+        <Route exact path="/resume" component={PageResume} />
+        <Route exact path="/projects" component={PageProjects} />
+        <Route exact path="/*" component={Page401} />
+      </Switch>
+    </Router>
+  </>
 
 export default App
